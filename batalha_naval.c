0d0f0a58e9fc5b5c94b59e6a8dbc007f1b373df8
LDJESD3-18 <leac162007@gmail.com>
2026-02-19 18:05:12 +0000

    codigo19

--- batalha_naval.c
+++ batalha_naval.c
@@ -33,17 +33,17 @@ int main (){                                                                    
     for (int i = 0 ;i<10;i++){                                              //Estrutura de repetição que cria e inicializa i e repete 10 vezes o código abaixo se i for menor que 10 e após a execução do código incrementa 1 a i. 
         printf("%d",linha[i]);                                            //Mostra o número que é o valor de cada linha baseado no valor de i, exemplo se  i for 0: mostra 1(linha 1)  
        for(int j=0;j<10;j++ ){
-       printf(" %d ",tabuleiro[j][0],tabuleiro[j][1],tabuleiro[j][2],tabuleiro[j][3],tabuleiro[j][4],tabuleiro[j][5],tabuleiro[j][6],tabuleiro[j][7],tabuleiro[j][9],tabuleiro[j][0],"\n");        //Mostra o número que é o valor de cada linha baseado no valor de t, exemplo se  t for 0: mostra 0(número que corresponde a A1,coluna A e linha 1) e assim por diante
+       printf(" %d ",tabuleiro[j][0],tabuleiro[j][1],tabuleiro[j][2],tabuleiro[j][3],tabuleiro[j][4],tabuleiro[j][5],tabuleiro[j][6],tabuleiro[j][7],tabuleiro[j][9],tabuleiro[j][0]);        //Mostra o número que é o valor de cada linha baseado no valor de t, exemplo se  t for 0: mostra 0(número que corresponde a A1,coluna A e linha 1) e assim por diante
        
-
-       }  
+       } 
 
 
        
 
 
                       
-       }  
+       } printf("\n");                       //Faz um espaçamento
+ 
       
     
       }  
