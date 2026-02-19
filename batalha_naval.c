c3b6416117e4092048d8a671ac01059d9a00652f
LDJESD3-18 <leac162007@gmail.com>
2026-02-19 00:15:19 +0000

    codigo02

--- batalha_naval.c
+++ batalha_naval.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 int main (){
-     char linha [10] ={"A","B","C","D","E","F","G","H","I","J"};
+     char linha [10] ={"A","B","C","D","E","F","G","H","I","J"};       
      
-     int tabuleiro[10][10];
+     int tabuleiro[10][10];                  //Cria o tabuleiro 10 x10
      for (int i=0;i<10;i++){
        printf("%c",linha[i]);
 
