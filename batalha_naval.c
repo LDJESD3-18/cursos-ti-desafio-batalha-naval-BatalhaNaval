3fffd4e4d31428a2c92b10564eac707b3217c93f
LDJESD3-18 <leac162007@gmail.com>
2026-02-19 00:14:03 +0000

    alteracao10

---
+++ batalha_naval.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+     char linha [10] ={"A","B","C","D","E","F","G","H","I","J",};
+     
+     int tabuleiro[10][10];
+     for (int i=0;i<10;i++){
+       printf("%c",linha[i]);
+
+
+     }
+
+
+
+
+
+}
