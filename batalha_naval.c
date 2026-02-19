1909151b6a2d8729b30e9ac0b5141ca9059879c3
LDJESD3-18 <leac162007@gmail.com>
2026-02-19 16:39:51 +0000

    Codigo06

--- batalha_naval.c
+++ batalha_naval.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
+#include <stdio.h>                                                                   //Função que acessa as bibliotecas para criar o arquivo executável
 
-int main (){
-     char linha [10] ={'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};       
+int main (){                                                                        //Função que faz o programa funcionar
+     char coluna [10] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};        //Vetor que armazena o valor de cada coluna do tabuleiro
+     int  linha  [10] = {1,2,3,4,5,6,7,8,9,10};                                   //Vetor que armazena o valor de cada linha do tabuleiro
      
-     int tabuleiro[10][10]={
+     int tabuleiro[10][10]={                                                    //Matriz 10 linhas por 10 colunas que armazena o tabuleiro sem navios,cada zero representa água no jogo
      {0,0,0,0,0,0,0,0,0,0},                     
      {0,0,0,0,0,0,0,0,0,0},                     
      {0,0,0,0,0,0,0,0,0,0},                    
@@ -16,14 +17,24 @@ int main (){
      {0,0,0,0,0,0,0,0,0,0},                    
      
      };
-     printf("Tabuleiro Batalha Naval \n");
+     printf("Tabuleiro Batalha Naval \n");                                 //Mostra A expressão Tabuleiro Batalha Naval
      
-     //Cria o tabuleiro 10 x10
+     
      for (int i = 0;i<10;i++){
-       printf("%c ",linha[i]);
+       printf("%c ",coluna[i]);
+
+
+      }
+    
+    printf("\n");
+
+    for (int j = 0;j<10;j++){
+       printf("%d\n",linha[j]);
+
 
 
      }              ////teste e ir dormir 
+
 
 
 
