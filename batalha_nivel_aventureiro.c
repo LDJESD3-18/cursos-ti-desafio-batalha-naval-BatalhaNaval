d8cb39d965dce5da9f7896bb45ac028d16e28584
LDJESD3-18 <leac162007@gmail.com>
2026-02-20 17:23:43 +0000

    codigo16

--- batalha_nivel_aventureiro.c
+++ batalha_nivel_aventureiro.c
@@ -38,13 +38,13 @@ int main (){                                                                    
      //Posicionar o navio3(diagonal principal)
      for (int i = 0 ;i<10;i++){                                                                                    //Estrutura de repetição que cria e inicializa i e repete 10 vezes o código abaixo se i for menor que 10 e após a execução do código incrementa 1 a i. 
         printf("%d ",linha[i]);                                                                                       //Mostra o número que é o valor de cada linha baseado no valor de i, exemplo se  i for 0: mostra 1(linha 1)  
-          for(int t=0; t<10;t++ ){                                                                                   //estrutura de repetição que cria e inicializa  t ,verifica se t for menor que 10 se sim executa o código abaixo e após a execução incrementa 1 a t. 
-          if(i== 0 && t==0 || i== 1 && t==1 || i== 2 && t==2 || i== 3 && t==3){                                     //Estrutura de repetição que verifica as posições A1,B2,C3 E D4 ,se o número da linha e da coluna são i==0 e t==0 ou i==1 e t==1 ou i==2 e t==2 ou i==3 e t==3 
-          tabuleiro[i][t] = 3;                                                                                     //Se pelo menos uma dessas condições for verdadeira coloca o 3 nessa posição
+          for(int j =0; j <10;j++ ){                                                                                   //estrutura de repetição que cria e inicializa  j ,verifica se j for menor que 10 se sim executa o código abaixo e após a execução incrementa 1 a t. 
+          if(i== 0 && j==0 || i== 1 && j==1 || i== 2 && j==2 || i== 3 && j==3){                                     //Estrutura de repetição que verifica as posições A1,B2,C3 E D4 ,se o número da linha e da coluna são i==0 e j==0 ou i==1 e j==1 ou i==2 e j==2 ou i==3 e j==3 
+          tabuleiro[i][j] = 3;                                                                                     //Se pelo menos uma dessas condições for verdadeira coloca o 3 nessa posição
 
 
 
-         printf("%d ",tabuleiro[i][t]);                                                                         //Mostra o 3 na posição A1 ou B2 ou C3 ou D4 
+         printf("%d ",tabuleiro[i][j]);                                                                         //Mostra o 3 na posição A1 ou B2 ou C3 ou D4 
             
          
             
@@ -52,18 +52,18 @@ int main (){                                                                    
                                                                                                   
         //Posicionar o navio4(diagonal secundária)
           
-          }else if(i==9 && t == 0||i==8 && t==1||i==7 && t==2 ||i==6 && t==3||i==5 && t==4){  //Estrutura de repetição que verifica as posições A10,B9,C8,D7,E6 ,se o número da linha e o  da coluna são  i==9 , t == 0 ou i==8 , t==1  ou i==7 , t==2 ou i==6 , t==3 ou i==5 , t==4
-             tabuleiro[i][t] = 3;                                                             //Se pelo menos uma dessas condições for verdadeira coloca o 3 nessa posição
+          }else if(i==9 && j == 0||i==8 && j==1||i==7 && j==2 ||i==6 && j==3||i==5 && j==4){  //Estrutura de repetição que verifica as posições A10,B9,C8,D7,E6 ,se o número da linha e o  da coluna são  i==9 , j == 0 ou i==8 , j==1  ou i==7 , j==2 ou i==6 , j==3 ou i==5 , j==4
+             tabuleiro[i][j] = 3;                                                             //Se pelo menos uma dessas condições for verdadeira coloca o 3 nessa posição
 
 
-            printf("%d ",tabuleiro[i][t]);                                                 //Mostra o 3 na posição A10 ou B9 ou C8 ou D7 ou E6 
+            printf("%d ",tabuleiro[i][j]);                                                 //Mostra o 3 na posição A10 ou B9 ou C8 ou D7 ou E6 
             
              
 
 
           } else{
             
-            printf("%d ",tabuleiro[i][t]);                                //Mostra o número que é o valor de cada linha e da coluna baseado no valor de t, exemplo se  t for 0: mostra 0(número que corresponde a A1,coluna A e linha 1) com espaçamento e assim por diante}
+            printf("%d ",tabuleiro[i][j]);                                //Mostra o número que é o valor de cada linha e da coluna baseado no valor de j, exemplo se  j for 0: mostra 0(número que corresponde a A1,coluna A e linha 1) com espaçamento e assim por diante}
         
     
         }   
